efe5303487df656f6ab0eac99f0ebe782b0def5d
Adham Akmal Azmi <adham_92@live.com>
2024-03-04 16:46:23 +0800

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import '@/styles/globals.css';
+
+import { ColorSchemeScript, MantineProvider, createTheme } from '@mantine/core';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+export const metadata: Metadata = {
+  title: 'Safwa Business Process',
+  description: 'Internal business process user interface for Safwa',
+};
+
+const theme = createTheme({
+  fontFamily: 'inherit',
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={inter.className}>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
