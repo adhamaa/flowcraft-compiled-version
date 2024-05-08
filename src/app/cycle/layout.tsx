1ed394f9274a3f1e99738087977901d42189ea3d
Adham Akmal Azmi <adham_92@live.com>
2024-05-08 17:54:22 +0800

    fix: remove session from components props

--- src/app/cycle/layout.tsx
+++ src/app/cycle/layout.tsx
@@ -8,12 +8,11 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth()
 
   return (
     <div className={clsx('grid grid-rows-[4rem_auto] h-screen transition-all duration-300 overflow-hidden')}
     >
-      <Header {...{ session }} />
+      <Header />
       {children}
     </div>
   );
