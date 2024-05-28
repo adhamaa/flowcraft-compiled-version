8e51a1cbe129779ab8ea049f9e6541cd23636cfd
Adham Akmal Azmi <adham_92@live.com>
2024-05-28 15:45:33 +0800

    style: change to button bg

--- src/components/form/SaveActions.tsx
+++ src/components/form/SaveActions.tsx
@@ -16,7 +16,7 @@ const SaveActions = ({ name, copyValue, disabled, onCancel, type }: {
 }) => {
   return (
     <div className={clsx(
-      'flex items-center ml-auto space-x-1',
+      'flex items-center ml-auto space-x-1.5',
       name === 'process_stage_name' && 'absolute right-0'
     )}>
       {!disabled &&
@@ -33,7 +33,11 @@ const SaveActions = ({ name, copyValue, disabled, onCancel, type }: {
               size="lg"
               radius="md"
               aria-label="Settings">
-              <Icon icon="heroicons-outline:check" width="1.2rem" className='border p-2 rounded-lg text-black/70 hover:text-black/50' />
+              <Icon
+                icon="heroicons-outline:check"
+                width="1.2rem"
+                className='border p-2 rounded-lg text-black/70 hover:text-black/50'
+              />
             </ActionIcon>
           </Tooltip>
           <Tooltip label="Cancel">
@@ -48,60 +52,69 @@ const SaveActions = ({ name, copyValue, disabled, onCancel, type }: {
               aria-label="Settings"
               onClick={onCancel}
             >
-              <Icon icon="heroicons-outline:x" width="1.2rem" className='border p-2 rounded-lg text-black/70 hover:text-black/50' />
+              <Icon
+                icon="heroicons-outline:x"
+                width="1.2rem"
+                className='border p-2 rounded-lg text-black/70 hover:text-black/50'
+              />
             </ActionIcon>
           </Tooltip>
         </>
       }
-      <div className="pl-1 flex items-center">
-        {type === 'json' &&
-          <>
-            <CopyButton value={copyValue as string} timeout={2000}>
-              {({ copied, copy }) => (
-                <Tooltip label={copied ? 'Copied' : 'Copy'}>
-                  <ActionIcon
-                    // disabled
-                    color={copied ? 'teal' : 'black'}
-                    variant="subtle"
-                    size="lg"
-                    radius="md"
-                    onClick={copy}
-                  >
-                    {copied ? (
-                      <Icon
-                        icon="heroicons-outline:check"
-                        width="1.2rem"
-                        height="1.2rem"
-                        className='text-black/70 hover:text-black/50'
-                      />
-                    ) : (
-                      <Icon
-                        icon="heroicons-outline:document-duplicate"
-                        width="1.2rem"
-                        height="1.2rem"
-                        className='text-black/70 hover:text-black/50'
-                      />
-                    )}
-                  </ActionIcon>
-                </Tooltip>
-              )}
-            </CopyButton>
-            <ActionIcon
-              disabled
-              color='black'
-              variant="subtle"
-              size="lg"
-              bg='transparent'
-              radius="md">
-              <Icon
-                icon="ph:code-bold"
-                width="1.2rem"
-                height="1.2rem"
-                className='cursor-default disabled:text-black/50'
-              />
-            </ActionIcon>
-          </>}
-      </div>
+      {/* <div className="flex items-center"> */}
+      {type === 'json' &&
+        <>
+          <CopyButton value={copyValue as string} timeout={2000}>
+            {({ copied, copy }) => (
+              <Tooltip label={copied ? 'Copied' : 'Copy'}>
+                <ActionIcon
+                  // disabled
+                  color='black'
+                  variant="transparent"
+                  bg={copied ? "#c5aff9" : '#F1F5F9'}
+                  size="lg"
+                  radius="md"
+                  onClick={copy}
+                >
+                  {copied ? (
+                    <Icon
+                      icon="heroicons-outline:check"
+                      width="1.2rem"
+                      height="1.2rem"
+                      className='text-black/70 hover:text-black/50'
+                    />
+                  ) : (
+                    <Icon
+                      icon="heroicons-outline:document-duplicate"
+                      width="1.2rem"
+                      height="1.2rem"
+                      className='text-black/70 hover:text-black/50'
+                    />
+                  )}
+                </ActionIcon>
+              </Tooltip>
+            )}
+          </CopyButton>
+          <ActionIcon
+            disabled
+            color='black'
+            variant="subtle"
+            size="lg"
+            bg='transparent'
+            radius="md"
+            classNames={{
+              root: 'disabled:!text-black/60'
+            }}
+          >
+            <Icon
+              icon="ph:code-bold"
+              width="1.2rem"
+              height="1.2rem"
+              className='cursor-default'
+            />
+          </ActionIcon>
+        </>}
+      {/* </div> */}
     </div>
   )
 };
