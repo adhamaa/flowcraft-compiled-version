c6e13e4ab68666eb35703bed38dadff9eb25f15c
adhamaa <adham_92@live.com>
2024-06-18 17:05:41 +0800

    feat: sync states

--- src/store/WorkInProgressDiagram/index.ts
+++ src/store/WorkInProgressDiagram/index.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import superjson from 'superjson' //  can use anything: serialize-javascript, devalue, etc.
 import { PersistStorage, persist } from 'zustand/middleware'
+import dagre from '@dagrejs/dagre';
 
 import {
   Connection,
@@ -19,13 +20,22 @@ import {
 import initialNodes from '@/components/reactflow/nodes';
 import initialEdges from '@/components/reactflow/edges';
 import { Apps_label, getDiagramData } from '@/lib/service/client';
+import { calculateNodePositions } from '@/components/reactflow/CalculateNodePositions';
+import { useShallow } from 'zustand/react/shallow';
 
-export type RFState = {
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 250; // horizontal space between nodes
+const nodeHeight = 200; // vertical space between nodes
+
+type RFState = {
   nodes: Node[];
   edges: Edge[];
   onNodesChange: OnNodesChange;
   onEdgesChange: OnEdgesChange;
   onConnect: OnConnect;
+  onLayout: (direction: string | undefined) => void;
   setNodes: (nodes: Node[]) => void;
   setEdges: (edges: Edge[]) => void;
   fetchNodesEdges: (props: { cycle_id: string; apps_label: Apps_label }) => Promise<void>;
@@ -44,8 +54,40 @@ const storage: PersistStorage<RFState> = {
   removeItem: (name) => localStorage.removeItem(name),
 }
 
+const getLayoutedElements = (nodes: Node[], edges: Edge[], direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
 
-// this is our useStore hook that we can use in our components to get parts of the store and call actions
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    //@ts-ignore
+    node.targetPosition = isHorizontal ? 'left' : 'top';
+    //@ts-ignore
+    node.sourcePosition = isHorizontal ? 'right' : 'bottom';
+
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+
+    return node;
+  });
+
+  return { nodes, edges };
+};
+
 const useDiagramStore = create<RFState>()(
   persist(
     (set, get) => ({
@@ -66,6 +108,15 @@ const useDiagramStore = create<RFState>()(
           edges: addEdge(connection, get().edges),
         });
       },
+      onLayout: (direction: string | undefined) => {
+        const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+          get().nodes,
+          get().edges,
+          direction
+        );
+
+        set({ nodes: layoutedNodes, edges: layoutedEdges })
+      },
       setNodes: (nodes: Node[]) => {
         set({ nodes });
       },
@@ -76,33 +127,28 @@ const useDiagramStore = create<RFState>()(
         cycle_id,
         apps_label,
       }) => {
-        const storedData = storage.getItem('diagram-storage');
         const diagramData = await getDiagramData({ cycle_id, apps_label });
-        set({
-          nodes: diagramData.nodes.map(({ position, ...node }: Node) => {
-            const removedPosition = { ...node, position: { x: 0, y: 0 } };
-            return {
-              ...node,
-              position: {
-                x: 0,
-                y: 0
-              },
-            }
-          }),
-          edges: diagramData.edges.map(({ type, markerEnd, ...edge }: Edge) => {
-            return {
-              ...edge,
-            }
-          }),
-
+        const editedNodes = diagramData.nodes.map(({ position, ...node }: Node) => {
+          return { ...node, position: { x: 0, y: 0 } } // remove position and set x, y to 0
         });
-        // }
+        const editedEdges = diagramData.edges.map(({ type, markerEnd, ...edge }: Edge) => {
+          return { ...edge } // remove type and markerEnd
+        });
+
+        const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+          editedNodes,
+          editedEdges
+        );
+
+        set({
+          nodes: layoutedNodes,
+          edges: layoutedEdges,
+        });
       },
       updateNodeColor: (nodeId: string, color: string) => {
         set({
           nodes: get().nodes.map((node) => {
             if (node.id === nodeId) {
-              // it's important to create a new object here, to inform React Flow about the changes
               node.data = { ...node.data, color };
             }
 
@@ -118,5 +164,17 @@ const useDiagramStore = create<RFState>()(
   ),
 );
 
-export default useDiagramStore;
+const useWorkInProgressDiagram = () => useDiagramStore(
+  useShallow((state: RFState) => ({
+    nodes: state.nodes,
+    edges: state.edges,
+    onNodesChange: state.onNodesChange,
+    onEdgesChange: state.onEdgesChange,
+    onConnect: state.onConnect,
+    onLayout: state.onLayout,
+    fetchNodesEdges: state.fetchNodesEdges,
+  })),
+);
+
+export default useWorkInProgressDiagram;
 
