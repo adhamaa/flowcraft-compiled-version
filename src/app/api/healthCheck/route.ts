b31ba5951828cdf91cd0089885386ec013b77734
Adham Akmal Azmi <adham_92@live.com>
2024-06-20 22:46:56 +0800

    fix: message

--- src/app/api/healthCheck/route.ts
+++ src/app/api/healthCheck/route.ts
@@ -1,7 +1,7 @@
 
 export async function GET(request: Request) {
   return new Response(JSON.stringify({
-    message: 'Test health check for port 3008 CMUI'
+    message: 'Test health check for port 3030 Flowcraft'
   }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' }
