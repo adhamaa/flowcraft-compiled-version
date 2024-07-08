f60dd63a839cd585a89693f96567f73cb1b1c874
Adham Akmal Azmi <adham_92@live.com>
2024-07-08 09:55:55 +0800

    fix: typo

--- src/app/cycle/restructure/[cycle_uuid]/_component/workspace/Wip.tsx
+++ src/app/cycle/restructure/[cycle_uuid]/_component/workspace/Wip.tsx
@@ -53,7 +53,7 @@ function Wip() {
 
   return (
     <div className='h-full space-y-6'>
-      <h1 className='text-xl font-semibold'>Current Cycle Diagram</h1>
+      <h1 className='text-xl font-semibold'>Work In Progress Diagram</h1>
       <div className='border border-black rounded-xl h-full'>
         <ReactFlowProvider>
           <ReactFlow
