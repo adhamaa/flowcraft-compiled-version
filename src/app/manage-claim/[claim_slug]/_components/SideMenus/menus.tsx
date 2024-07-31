12dcfdca87f0d3cb110d74e6556d581aaf61b795
Adham Akmal Azmi <adham_92@live.com>
2024-07-31 18:05:22 +0800

    saving

--- src/app/manage-claim/[claim_slug]/_components/SideMenus/menus.tsx
+++ src/app/manage-claim/[claim_slug]/_components/SideMenus/menus.tsx
@@ -74,39 +74,52 @@ function SideMenus() {
 
   const sideMenuList = [
     {
-      name: 'Cycle', value: 'cycle', icon: 'heroicons-outline:refresh',
+      name: 'Manage Claim', value: 'manage-claim', icon: 'heroicons-outline:refresh',
       children: [
         {
           name: 'General Information',
           value: 'general',
-          onClick: () => router.push(`/cycle/${cycle_id}/` + '?' + remainQueryString()),
+          onClick: () => { }
+          // onClick: () => router.push(`/cycle/${cycle_id}/` + '?' + remainQueryString()),
         },
         {
-          name: 'Stages',
-          value: 'stages',
-          onClick: async () => {
-            router.push(`/cycle/${cycle_id}/stage/${stageData?.[0]?.stage_uuid}/` + '?' + remainQueryString());
-          },
-          onChange: async (value: any) => {
-            router.push(`/cycle/${cycle_id}/stage/${value}/` + '?' + remainQueryString());
-          },
-          children: stageData?.map((stage) => ({
-            name: stage.stage_name,
-            value: stage.stage_uuid,
-          }))
+          name: 'Pending Claims',
+          value: 'pending-claims',
+          onClick: () => { }
+          // onClick: async () => {
+          //   router.push(`/cycle/${cycle_id}/stage/${stageData?.[0]?.stage_uuid}/` + '?' + remainQueryString());
+          // },
+          // onChange: async (value: any) => {
+          //   router.push(`/cycle/${cycle_id}/stage/${value}/` + '?' + remainQueryString());
+          // },
+          // children: stageData?.map((stage) => ({
+          //   name: stage.stage_name,
+          //   value: stage.stage_uuid,
+          // }))
         },
         {
-          name: 'Deleted Stage',
-          value: 'deleted_stage',
-          onClick: () => router.push(`/cycle/${cycle_id}/stage/deleted/${deletedStageData?.[0]?.stage_uuid}/` + '?' + remainQueryString()),
-          onChange: async (value: any) => {
-            router.push(`/cycle/${cycle_id}/stage/deleted/${value}/` + '?' + remainQueryString());
-          },
-          children: deletedStageData?.map((stage) => ({
-            name: stage.stage_name,
-            value: stage.stage_uuid,
-          }))
+          name: 'Completed Claims',
+          value: 'completed-claims',
+          onClick: () => { }
+          // onClick: () => router.push(`/cycle/${cycle_id}/stage/deleted/${deletedStageData?.[0]?.stage_uuid}/` + '?' + remainQueryString()),
+          // onChange: async (value: any) => {
+          //   router.push(`/cycle/${cycle_id}/stage/deleted/${value}/` + '?' + remainQueryString());
+          // },
+          // children: deletedStageData?.map((stage) => ({
+          //   name: stage.stage_name,
+          //   value: stage.stage_uuid,
+          // }))
         },
+        {
+          name: 'stage',
+          value: 'stage',
+          onClick: () => { }
+        },
+        {
+          name: 'user',
+          value: 'user',
+          onClick: () => { }
+        }
       ]
     },
   ];
@@ -164,15 +177,18 @@ function SideMenus() {
                   <TabsList>
                     <></>
                     {menu.children.map((child) => (
-                      <TabsTab key={child.value} value={child.value} onClick={child.onClick}>
+                      <TabsTab
+                        key={child.value}
+                        value={child.value}
+                        onClick={child?.onClick}>
                         {child.name}
                       </TabsTab>
                     ))}
                   </TabsList>}
 
-                {menu.children.map((child) => (
+                {/* {menu.children.map((child) => (
                   <TabsPanel key={child.value} value={child.value} >
-                    {child.children
+                    {child?.children
                       && (
                         <Tabs
                           value={stage_uuid as string}
@@ -182,7 +198,7 @@ function SideMenus() {
                             tab: 'w-40 !border-r-0 !border-l-4 !rounded-none data-[active=true]:!border-[#895CF3] ml-4 my-4 !pl-1 hover:bg-transparent data-[active=true]:font-semibold',
                             tabLabel: '~text-md/lg',
                           }}
-                          onChange={child.onChange}
+                          onChange={child?.onChange}
                         >
                           {renderStageData?.length === 0 && <div className='flex justify-start items-start p-7 h-full w-max italic'>No stages found</div>}
                           {!isSideMenuCollapse
@@ -191,7 +207,7 @@ function SideMenus() {
                               <TabsList>
                                 <></>
                                 <ScrollAreaAutosize mah={768}>
-                                  {child.children?.map((stage, i) => (
+                                  {child?.children?.map((stage, i) => (
                                     <Tooltip key={i} label={stage.name} position='right'>
                                       <TabsTab
                                         key={stage.value}
@@ -216,7 +232,7 @@ function SideMenus() {
                         </Tabs>
                       )}
                   </TabsPanel>
-                ))}
+                ))} */}
               </Tabs>
             </TabsPanel>
           )
