eedb319c7b1045fe0970fce4dd5dce56c17cdd45
Adham Akmal Azmi <adham_92@live.com>
2024-07-31 14:56:27 +0800

    feat: footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import * as React from 'react'
+
+function Footer() {
+  return (
+    <footer className='flex justify-between items-center h-16 px-40 py-12'>
+      <div>
+        <span>v2.0.0-1 | 30 July 2024</span>
+      </div>
+      <div>
+        <span>Designed by EZDOM Technology</span>
+        <br />
+        <span>Deployed by Schinkels Technik</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
