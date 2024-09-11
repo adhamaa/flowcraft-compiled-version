16c9f005b3fcad0feaa3df8bc5912612db1d72a8
Adham Akmal Azmi <adham_92@live.com>
2024-09-11 11:51:37 +0800

    feat: .sh file

--- buildFlowcraft.sh
+++ buildFlowcraft.sh
@@ -5,6 +5,7 @@
 shopt -s extglob
 
 nvm use 18.17.1
+cp .env.example.qa .env
 # checking for the build all etender
 if [ "$select" != "skipenv" ]; then
     nano .env
@@ -12,27 +13,26 @@ fi
 
 pnpm clean
 pnpm i
-cp .env.example.qa .env
 pnpm build:server
 rm -rfv !(.env|package.json|dist|public|.next)
 rm -rf .env.example.prod
 rm -rf .env.example.qa
 rm -rf .eslintrc.json
-rm -rf .git
-rm -rf .gitignore
+# rm -rf .git
+# rm -rf .gitignore
 rm -rf .husky
 rm -rf .vscode
 pnpm i
 
-pm=$(pm2 ls -m | grep "+--\|status" | grep "flowcraft")
-# checking to create new pm2 or use old pm2
-if [ "$pm" == "+--- flowcraft" ]; then
-    # checking if there are already have pm2 for flowcraft
-    pm2 restart flowcraft --time
-else
-    # create new pm2 for flowcraft
-    pm2 start pnpm --name flowcraft -- start:server
-    pm2 restart flowcraft --time
-fi
+# pm=$(pm2 ls -m | grep "+--\|status" | grep "flowcraft")
+# # checking to create new pm2 or use old pm2
+# if [ "$pm" == "+--- flowcraft" ]; then
+#     # checking if there are already have pm2 for flowcraft
+#     pm2 restart flowcraft --time
+# else
+#     # create new pm2 for flowcraft
+#     pm2 start pnpm --name flowcraft -- start:server
+#     pm2 restart flowcraft --time
+# fi
 
-nvm use 16.15.1
+# nvm use 16.15.1
