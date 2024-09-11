4912ed31b4f69c41a1a246fab03e277824c81a96
Adham Akmal Azmi <adham_92@live.com>
2024-09-11 13:04:52 +0800

    feat: bash script for compiling and add remote to be pushed to

--- buildFlowcraft.sh
+++ buildFlowcraft.sh
@@ -1,38 +1,35 @@
-#! /usr/bin/bash
-#this script is for temporary use because Adham found a bug that need to be fixed by react dev
+#!/usr/bin/bash
+# This script is for temporary use because Adham found a bug that needs to be fixed by the React dev.
 
 # Enable extended globbing
 shopt -s extglob
 
-nvm use 18.17.1
-cp .env.example.qa .env
-# checking for the build all etender
-if [ "$select" != "skipenv" ]; then
-    nano .env
-fi
-
+# Clean and build in Repo A
 pnpm clean
-# pnpm i
 pnpm build:server
+
+# Remove unnecessary files after build
 rm -rfv !(.env|package.json|dist|public|.next)
 rm -rf .env.example.prod
 rm -rf .env.example.qa
 rm -rf .eslintrc.json
-# rm -rf .git
-# rm -rf .gitignore
 rm -rf .husky
 rm -rf .vscode
-pnpm i
 
-# pm=$(pm2 ls -m | grep "+--\|status" | grep "flowcraft")
-# # checking to create new pm2 or use old pm2
-# if [ "$pm" == "+--- flowcraft" ]; then
-#     # checking if there are already have pm2 for flowcraft
-#     pm2 restart flowcraft --time
-# else
-#     # create new pm2 for flowcraft
-#     pm2 start pnpm --name flowcraft -- start:server
-#     pm2 restart flowcraft --time
-# fi
+# Install dependencies after cleanup
+# pnpm i
 
-# nvm use 16.15.1
+# Check if the 'compiled' remote already exists; if not, add it
+if git remote get-url compiled > /dev/null 2>&1; then
+    echo "Remote 'compiled' already exists."
+else
+    git remote add compiled https://github.com/adhamaa/flowcraft-compiled-version.git
+    echo "Remote 'compiled' added."
+fi
+
+# Push compiled code to the 'compiled' remote repository
+git add .
+git commit -m "Build: push compiled code"
+git push -u compiled main --force
+
+echo "Compiled code pushed to 'compiled' remote: https://github.com/adhamaa/flowcraft-compiled-version.git"
